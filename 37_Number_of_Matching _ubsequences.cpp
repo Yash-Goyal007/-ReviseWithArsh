0a639a7fd1f6858ccf394d7a98aba755016e623a
Yash Goyal <yashgoyalmrt1912@gmail.com>
2023-01-21 22:10:00 +0530

    Create 37_Number_of_Matching _ubsequences.cpp

---
+++ 37_Number_of_Matching _ubsequences.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numMatchingSubseq(string s, vector<string>& words) {
+        vector<vector<int>> hash (26);
+		for (int i = 0; i < s.size (); ++i) 
+            hash[s[i] - 'a'].push_back (i);
+		int ans = 0;
+		for (auto& word : words) {
+			int x = -1;
+			bool found = true;
+			for (auto i : word) {
+				auto temp = upper_bound (hash[i - 'a'].begin (), hash[i - 'a'].end (), x);
+				if (temp == hash[i - 'a'].end ()) found = false;
+				else x = *temp;
+			}
+			if (found) 
+                ans++;
+		}
+
+		return ans;
+    }
+};

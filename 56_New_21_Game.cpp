f8c85fbd0a55c1900cb0d16d7de8f81aa9225a3e
Yash Goyal <yashgoyalmrt1912@gmail.com>
2023-01-24 00:30:38 +0530

    Create 56_New_21_Game.cpp

---
+++ 56_New_21_Game.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double new21Game(int n, int k, int maxPts) {
+        if(k==0 || n>=k+maxPts-1)
+            return (double) 1;
+        vector<double> dp(n+1);
+        dp[0]=1;
+        double sum = 0;
+        for(int i=0; i<n; i++){
+            if(i<k)
+                sum+=dp[i]; 
+            if(i>=maxPts)
+                sum-=dp[i-maxPts];
+            dp[i+1] = sum/maxPts;
+        }
+        double res = 0;
+        for(int i=k; i<=n; i++)
+            res+=dp[i];
+        return res;
+    }
+};

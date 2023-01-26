3c67a9bb8dbe1ea32246c4792389876163329c6e
Yash Goyal <yashgoyalmrt1912@gmail.com>
2023-01-27 01:31:51 +0530

    Create 68_Random_Pick_With_Weight.cpp

---
+++ 68_Random_Pick_With_Weight.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    vector<int> v;
+public:
+    Solution(vector<int>& w) {
+        v.push_back(w[0]);
+        for(int i=1;i<w.size();i++)
+            v.push_back(v[i-1]+w[i]);
+    }
+    
+    int pickIndex() {
+        int n= rand()%v[v.size()-1];
+        auto temp=upper_bound(v.begin(),v.end(),n);
+        return temp-v.begin();
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(w);
+ * int param_1 = obj->pickIndex();
+ */

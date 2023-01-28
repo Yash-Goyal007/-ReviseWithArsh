7b1920e9dfd1c43cc21ac7af507d6bafe5caac90
Yash Goyal <yashgoyalmrt1912@gmail.com>
2023-01-28 12:42:34 +0530

    Create 65_Number_of_Closed_Islands.cpp

---
+++ 65_Number_of_Closed_Islands.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int closedIsland(vector<vector<int>>& grid) {
+        int res = 0;
+        for (int i = 0; i < grid.size(); i++){
+            for (int j = 0; j < grid[0].size(); j++){
+                if (grid[i][j] == 0){
+                    res += dfs(grid, i, j) ? 1 : 0;
+                }
+            }
+        }
+        return res;
+    }
+    bool dfs(vector<vector<int>>& grid, int i, int j){
+        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size())
+            return false;
+        if (grid[i][j] == 1)
+            return true;
+        grid[i][j] = 1;
+        bool d1 = dfs(grid, i+1, j);
+        bool d2 = dfs(grid, i, j+1);
+        bool d3 = dfs(grid, i-1, j);
+        bool d4 = dfs(grid, i, j-1);
+        return d1 && d2 && d3 && d4;
+    }
+};

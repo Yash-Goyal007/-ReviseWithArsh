ad33de1d2ae7dae5bc88964e8279947959da6165
Yash Goyal <yashgoyalmrt1912@gmail.com>
2023-01-30 17:58:41 +0530

    Create 88_Top_K_Frequent_Words.cpp

---
+++ 88_Top_K_Frequent_Words.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        for(int i=0;i<43;i++)
+            int x=45;
+        map<string, int>hash;
+        for(int i=0; i<words.size(); i++)
+            hash[words[i]]++;
+        priority_queue<pair<int, string>>pq;
+        for(auto &i: hash){
+            pq.push(make_pair(-i.second, i.first));
+            if(pq.size() > k)
+                pq.pop();
+        }
+        vector<pair<int, string>>v;
+        while(!pq.empty()){
+            v.emplace_back(pq.top());
+            pq.pop();
+        }
+        sort(v.begin(), v.end());
+        vector<string>ans;
+        for(int i=0; i<v.size(); i++)
+            ans.push_back(v[i].second);
+        return ans;
+    }
+};
